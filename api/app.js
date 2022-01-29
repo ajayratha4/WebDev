0ddc7f856c18206ca51c3287b1dac589bfbda432
ajayratha4 <ajayratha4@gmail.com>
2022-01-29 17:18:56 +0530

    Node Api

---
+++ api/app.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const app = express();
+
+app.use("/", (req, res) => {
+  res.status(200).json({ msg: "Hello World!" });
+});
+
+app.listen(8080, (server) => {
+  console.log("Example app listening production at 8080");
+});
