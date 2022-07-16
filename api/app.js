771b9be1e34be796e0d1d069a62eab8d42334588
ajayratha4 <73908312+ajayratha4@users.noreply.github.com>
2022-07-16 14:18:47 +0530

    tt

--- api/app.js
+++ api/app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.use("/", (req, res) => {
-  res.status(200).json({ msg: "Hello Upasana!" });
+  res.status(200).json({ msg: "Hello !" });
 });
 
 app.listen(8080, (server) => {
